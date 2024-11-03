53879a4e86ed02b71142bb2342007db5f15c728e
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2024-11-03 15:02:27 +0300

    final update

--- tictactoe/app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ tictactoe/app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TableRow;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -27,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         table = findViewById(R.id.board);
         for (int i=0; i<3;i++){
@@ -39,6 +39,44 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+
+        outState.putBoolean("player1Turn",player1Turn);
+        byte[] boardSingle = new byte[9];
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                boardSingle[3*i+j]=board[i][j];
+            }
+        }
+        outState.putByteArray("board",boardSingle);
+        super.onSaveInstanceState(outState);
+    }
+
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+        byte[] boardSingle = savedInstanceState.getByteArray("board");
+        for (int i = 0; i < 9; i++) {
+            board[i / 3][i % 3] = boardSingle[i];
+
+        }
+        TableLayout table = findViewById(R.id.board);
+        for (int i = 0; i < 3; i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            for (int j = 0; j < 3; j++) {
+                Button button = (Button) row.getChildAt(j);
+                if (board[i][j] == 1) {
+                    button.setText("X");
+                } else if (board[i][j] == 2) {
+                    button.setText("O");
+                }
+
+            }
+        }
     }
 
     class CellListener implements View.OnClickListener{
@@ -97,6 +135,61 @@ public class MainActivity extends AppCompatActivity {
             if (win){
                 return symbol;
             }
+
+            // check row
+            win = true;
+            for (int j = 0; j < 3; j++) {
+                if (board[row][j] != symbol) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return symbol;
+            }
+
+            if (row == col) {
+                win = true;
+                for (int i = 0; i < 3; i++) {
+                    if (board[i][i] != symbol) {
+                        win = false;
+                        break;
+                    }
+                }
+                if (win) {
+                    return symbol;
+                }
+            }
+
+            // Check anti-diagonal
+            if (row + col == 2) {
+                win = true;
+                for (int i = 0; i < 3; i++) {
+                    if (board[i][2-i] != symbol) {
+                        win = false;
+                        break;
+                    }
+                }
+                if (win) {
+                    return symbol;
+                }
+            }
+
+            // Check for draw
+            boolean isDraw = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == 0) {
+                        isDraw = false;
+                        break;
+                    }
+                }
+            }
+            if (isDraw) {
+                return 0; // Draw condition
+            }
+
+
             return -1;
         }
 
