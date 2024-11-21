96fdd38f6f2b3f0d56561225340d0f4ab25d2cec
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2024-11-21 12:05:15 +0300

    new version tictactoe

---
+++ tictactoeMV/app/src/main/java/com/example/tictactoemv/BoardPresenter.java
@@ -0,0 +1,61 @@
+package com.example.tictactoemv;
+
+import android.util.Log;
+import android.view.View;
+
+public class BoardPresenter implements BoardListener {
+    private final BoardView boardView;
+    private final Board board;
+
+    public BoardPresenter(BoardView boardView) {
+        this.boardView = boardView;
+        board = new Board(this);
+    }
+
+    @Override
+    public void playedAt(byte player, byte row, byte col) {
+        if (player == BoardListener.PLAYER_1){
+            boardView.putSymbol(BoardView.PLAYER_1_SYMBOL,row,col);
+        }
+        else{
+            boardView.putSymbol(BoardView.PLAYER_2_SYMBOL,row,col);
+        }
+    }
+
+    @Override
+    public void gameEnded(byte winner) {
+        switch (winner) {
+            case BoardListener.NO_ONE:
+                boardView.gameEnded(BoardView.DRAW);
+            case BoardListener.PLAYER_1:
+                boardView.gameEnded(BoardView.PLAYER_1_WINNER);
+            case BoardListener.PLAYER_2:
+                boardView.gameEnded(BoardView.PLAYER_2_WINNER);
+        }
+    }
+
+    @Override
+    public void invalidPlay(byte row, byte col) {
+        boardView.invalidPlay(row,col);
+    }
+
+    public void move(byte row,byte col){
+        board.move(row,col);
+    }
+
+    static class CellClickListener implements View.OnClickListener {
+        BoardPresenter boardPresenter;
+        byte row,col;
+
+        public CellClickListener(BoardPresenter boardPresenter, byte row, byte col) {
+            this.boardPresenter = boardPresenter;
+            this.row = row;
+            this.col = col;
+        }
+
+        public void onClick(View v){
+            Log.d("CellClickListener","at" + row + ","+col);
+            boardPresenter.move(row,col);
+        }
+    }
+}
