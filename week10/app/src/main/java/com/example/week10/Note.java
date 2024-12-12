cf81dccc296825b7e94bf66824adf3ffbb1ae634
U220709704 <kamalmialqedra@posta.mu.edu.tr>
2024-12-12 12:20:33 +0300

    week10

---
+++ week10/app/src/main/java/com/example/week10/Note.java
@@ -0,0 +1,46 @@
+package com.example.week10;
+
+import android.provider.ContactsContract;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Note implements Serializable {
+    private String header;
+    private Date date;
+    private String filePath;
+
+    public Note(String header, Date date, String filePath) {
+        this.header = header;
+        this.date = date;
+        this.filePath = filePath;
+    }
+
+    public Note() {
+
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
